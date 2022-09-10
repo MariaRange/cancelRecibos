f0a8a13945d6ab03d79c295409d29f5ab39e4337
MariaRange <ticqraeg001125mdgnsda1@gmail.com>
2022-09-09 21:08:56 -0500

    Primer Vista
    Primer vista de pantalla Cancelar Recibos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputsComponent } from './inputs/inputs.component';
+import { BotonComponent } from './boton/boton.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputsComponent,
+    BotonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/boton/boton.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-boton',
+  templateUrl: './boton.component.html',
+  styleUrls: ['./boton.component.css']
+})
+export class BotonComponent{
+}
